93cf779518b93fc8dc71752418ea9b8bf07b57b9
ethanluoyc <ethanluoyc@gmail.com>
2016-07-04 09:57:29 +0800

    Improve Danmu architechture.

--- danmu.py
+++ danmu.py
@@ -1,16 +1,42 @@
 # -*- coding: utf-8 -*-
+import os
+from flask import Flask, render_template, request, flash, jsonify
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask, render_template, request, flash
-from flask.ext.socketio import SocketIO, emit
+import config
 
-# import logging
-#
-# logging.basicConfig()
-
+CONFIG = config.ProdConfig if os.environ.get('DANMU_SERVER_ENV') == 'prod' \
+    else config.DevConfig
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cldds_safeword'
-app.config['DEBUG'] = True
+app.config.from_object(CONFIG)
+
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
+
+STATUS_FLAGS = {
+    'waiting': 0,
+    'approved': 1,
+    'disproved': 2
+}
+
+
+class Danmaku(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    msg = db.Column(db.String(80))
+    status = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, msg):
+        self.msg = msg
+        self.status = STATUS_FLAGS['waiting']
+
+    def to_dict(self):
+        return dict(id=self.id, data=self.msg, status=self.status)
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get(id)
 
 
 @app.route('/')
@@ -20,17 +46,39 @@ def index():
 
 @app.route('/post', methods=['GET', 'POST'])
 def post_message():
+
+    def mk_danmaku(msg):
+        d = Danmaku(msg)
+        db.session.add(d)
+        db.session.commit()
+        return d
+
     if request.method == 'POST':
         msg = request.form['message']
         if msg:
-            if "master" in request.form:
-                socketio.emit('post danmu', {'data': msg}, namespace='/post')
-                return ''
-            if len(msg) > 80:
+            if len(msg) <= 80:
+                danmaku = mk_danmaku(msg)
+                if "master" in request.form:
+                    socketio.emit('post danmu',
+                                  danmaku.to_dict(),
+                                  namespace='/post')
+                    return 0, 200
+                socketio.emit('check danmu',
+                              danmaku.to_dict(),
+                              namespace='/check')
+                if request.headers['X-Requested-With'] == 'XMLHttpRequest':
+                    print request.headers['Content-Type']
+                    return jsonify({'status': 0,
+                                    'messages': [{'body': 'Post succeeded :)',
+                                                  'category': 'success'}]}), 200
+                flash(u'Post succeeded :)', 'success')
+            else:
                 flash(u'The message is too long, max length is 20', 'danger')
                 return render_template('post_form.html')
-            socketio.emit('check danmu', {'data': msg}, namespace='/check')
-            flash(u'Post succeeded :)', 'success')
+        return jsonify({'status': 0,
+                        'messages': [{'body': 'Cannot be empty',
+                                      'category': 'danger'}]}), 400
+
     return render_template('post_form.html')
 
 
@@ -43,16 +91,28 @@ def check_danmu():
 def approve_danmu(msg):
     app.logger.info(msg)
     if 'data' in msg:
+        d = Danmaku.get_by_id(msg['id'])
+        d.status = STATUS_FLAGS['approved']
+        d.save()
         socketio.emit('post danmu', {'data': msg['data']}, namespace='/post')
 
 
+@socketio.on('disprove danmu', namespace='/check')
+def disprove_danmu(msg):
+    app.logger.info(msg)
+    if 'data' in msg:
+        d = Danmaku.get_by_id(msg['id'])
+        d.status = STATUS_FLAGS['disproved']
+        d.save()
+
+
 @socketio.on('connect', namespace='/post')
-def test_connect():
+def post_connect():
     app.logger.info('Board posting connected')
 
 
 @socketio.on('connect', namespace='/check')
-def test_check_connect():
+def check_connect():
     app.logger.info('Checking connected')
 
 # @socketio.on('disconnect', namespace='/post')
@@ -60,4 +120,5 @@ def test_check_connect():
 #     print('Client disconnected')
 
 if __name__ == '__main__':
+    db.create_all()
     socketio.run(app)
