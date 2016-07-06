5aaf987e115439806b56fb556234e6d32cc9f814
ethanluoyc <ethanluoyc@gmail.com>
2016-07-06 22:27:42 +0800

    Put keyboard short cuts into separate js file
    And fix linting problems with the js

--- static/js/danmu.js
+++ static/js/danmu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var danmuss = {}
+  var danmuss = {};
   // Settings for the Danmu Player!
   var options = {
     left: 0,
@@ -15,30 +15,31 @@ $(document).ready(function() {
     font_size_big: 70,
     opacity: 1.0,
     top_botton_danmu_time: 6000
-  }
+  };
 
   function _randomChoice(arr) {
-    return arr[Math.floor(arr.length * Math.random())]
+    return arr[Math.floor(arr.length * Math.random())];
   }
 
   //initialize danmu player
-  $('#danmu').danmu(options)
+  $('#danmu').danmu(options);
 
-  var danmuPlayer = $('#danmu') // a selector for the danmu div
+  // a selector for the danmu div
+  var danmuPlayer = $('#danmu');
 
   function show_danmu(danmuPlayer, msg) {
     //console.log(msg);
-    var heig = danmuPlayer.height() / 1.5
-    var row_conut = parseInt(heig / options.font_size_big)
-    var row = parseInt(row_conut * Math.random())
-    var top_local = (row) * options.font_size_big
+    var heig = danmuPlayer.height() / 1.5;
+    var row_conut = parseInt(heig / options.font_size_big);
+    var row = parseInt(row_conut * Math.random());
+    var top_local = (row) * options.font_size_big;
 
-    var a_danmu = '<div class=\'flying flying2\' id=\'linshi\'></div>'
-    danmuPlayer.append(a_danmu)
-    var fly_tmp_name = 'fly' + parseInt(heig * Math.random()).toString()
+    var a_danmu = '<div class=\'flying flying2\' id=\'linshi\'></div>';
+    danmuPlayer.append(a_danmu);
+    var fly_tmp_name = 'fly' + parseInt(heig * Math.random()).toString();
 
-    $('#linshi').attr('id', fly_tmp_name)
-    $('#' + fly_tmp_name).text(msg)
+    $('#linshi').attr('id', fly_tmp_name);
+    $('#' + fly_tmp_name).text(msg);
     $('#' + fly_tmp_name).css({
       'position': 'absolute',
       'top': top_local,
@@ -47,32 +48,72 @@ $(document).ready(function() {
       'white-space': 'nowrap',
       'color': 'white',
       'opacity': 1.0
-    })
+    });
     $('#' + fly_tmp_name).animate({
       left: -danmuPlayer.width() * 3
     },
       options.speed,
       function() {
-        $(this).remove()
-      })
+        $(this).remove();
+      });
   }
 
-  var namespace = '/post'
+  var namespace = '/post';
 
   // the socket.io documentation recommends sending an explicit package upon connection
-  var socket = io.connect('//' + document.domain + ':' + location.port + namespace)
+  var socket = io.connect('//' + document.domain + ':' + location.port + namespace);
 
   socket.on('connect', function() {
-    console.log('connected')
-  })
+    console.log('connected');
+  });
 
   socket.on('disconnect', function() {
-    console.log('disconnected')
-  })
+    console.log('disconnected');
+  });
 
   // receive and display danmu
   socket.on('post danmu', function(msg) {
-    console.log('received: ' + msg.data)
-    show_danmu(danmuPlayer, msg.data)
-  })
-})
+    console.log('received: ' + msg.data);
+    show_danmu(danmuPlayer, msg.data);
+  });
+});
+
+
+// Bind keyboard shortcuts
+$(document).ready(function() {
+  $(document).bind('keydown', 'd', function() {
+    if ($('#danmuholder').css("display") == 'none') {
+      $('#danmuholder').fadeIn("slow", function() {
+        $("#board-logo").fadeIn("slow", function() {
+          $(".url-footer").fadeIn("slow");
+        });
+      });
+    } else {
+      $("#board-logo").fadeOut("slow", function() {
+        $('#danmuholder').fadeOut("slow");
+      });
+      $(".url-footer").fadeOut("slow");
+
+    }
+
+  });
+
+  $(document).bind('keydown', 'n', function() {
+    $(".url-footer").fadeToggle("slow");
+  });
+  // Toggle opacity of the Danmuplayer, use animate rather than fade to avoid texts from inner divs from being affected by the changes in opacity
+  $(document).bind('keypress', 'j', function() {
+    console.log('pressed');
+    $("#danmu").animate({
+      backgroundColor: "rgba(0,0,0,1)"
+    }, 600);
+  });
+
+  $(document).bind('keypress', 'k', function() {
+    console.log("pressed");
+    $("#danmu").animate({
+      backgroundColor: "rgba(0,0,0,0.7)"
+    }, 600);
+  });
+
+});
